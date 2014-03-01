9a4e29d1c5b097499a809270eaef2067a9c907b6
Heiko Frost <heiko.frost@gmail.com>
2014-02-28 22:51:26 -0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Gassion::Application.config.secret_key_base = 'adfb7d880a9ff854c823ffb464a41423d8b30b71571480fd4a580a3104659b62a5e3f20eb105fa31907780ae0dd6b85a5eadd371ff8bc2c2d18c4739cc7a9109'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Gassion::Application.config.session_store :cookie_store, key: '_gassion_session'
